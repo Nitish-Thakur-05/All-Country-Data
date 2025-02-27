938748c978deb3bd78d19190287b56a0c1053f11
Nitish Thakur <tnitish440@gmail.com>
2025-02-27 21:58:01 +0530

    all file added

---
+++ country.js
@@ -0,0 +1,130 @@
+// getting data from url
+const queryData = new URLSearchParams(window.location.search).get('name')
+
+const countryName = document.querySelector('.country-name')
+const flag = document.querySelector('.flag-img')
+const nativeName = document.querySelector('.native-name')
+const population = document.querySelector('.population')
+const region = document.querySelector('.region')
+const subRegion = document.querySelector('.sub-region')
+const capital = document.querySelector('.capital')
+const domain = document.querySelector('.domain')
+const currency = document.querySelector('.currency')
+const languages = document.querySelector('.languages')
+const border = document.querySelector('.border')
+const backButton = document.querySelector('.back-btn')
+const spinnerContainer = document.querySelector('.spinner-container')
+const spinnerText = document.querySelector('.spinner-container h2')
+const spinner = document.querySelector('.spinner')
+const dataContainer = document.querySelector('.container')
+
+setTimeout(() => {
+    fetch(`https://restcountries.com/v3.1/name/${queryData}`).then((countryData)=> {
+        if(countryData.status != 200) {
+            throw new Error(`ERROR CODE ${countryData.status}`)
+        }
+        return countryData.json()
+    }).then(([fetchedData])=> {
+        spinnerContainer.style.display = 'none'
+        spinner.remove()
+        dataContainer.style.display = 'flex'
+    
+        flag.src = fetchedData.flags.svg || fetchedData.flags.png
+        countryName.textContent = fetchedData.name.common
+        nativeName.innerHTML = `
+                                    <b>Native Name: </b> ${fetchedData.name.nativeName ? Object.values(fetchedData.name.nativeName)[0]?.common : 'N/A'}
+        `
+        population.innerHTML = `
+                                <b>Population: </b> ${fetchedData.population.toLocaleString('en-IN')}
+        `
+        region.innerHTML = `
+                                <b>Region: </b> ${fetchedData.region}
+        `
+        subRegion.innerHTML = `
+                                <b>Sub Region: </b> ${fetchedData.subregion || "N/A"}  
+        `
+        capital.innerHTML = `
+                                <b>capital: </b> ${fetchedData.capital || "N/A"}
+        `
+        domain.innerHTML = `
+                                <b>Top Level Domain: </b> ${fetchedData.tld}
+        `
+        currency.innerHTML = `
+                                <b>currencies: </b> ${Object.values(fetchedData.currencies)[0].name}
+        `
+        languages.innerHTML = `
+                                <b>languages: </b> ${Object.values(fetchedData.languages)}
+    
+        `
+      // for border countries 
+      if(fetchedData.borders) {
+        const countryByCode = fetchedData.borders.map((curr) => {
+            return fetch(`https://restcountries.com/v3.1/alpha/${curr}`).then((data) => {
+                 return data.json()
+             })
+         })
+         Promise.all(countryByCode).then((data) => {
+             const fregment = document.createDocumentFragment()   
+             data.forEach(([curr]) => {
+                 const linkTag = document.createElement('a')
+                 linkTag.href = `country.html?name=${curr.name.common}`
+                 linkTag.innerHTML = `${curr.name.common}`
+                 fregment.appendChild(linkTag)
+             });
+             border.appendChild(fregment)
+         }).catch((err) => {
+             spinner.remove()
+             spinnerText.textContent = 'An Error Occured Unable to Fetch Data.....'
+         })
+    } else {
+        let borderHTML = `<b>Border Countries: &nbsp;</b><span>N/A</span>`
+        border.innerHTML = borderHTML
+    }
+
+    }).catch((err)=> {
+        spinner.remove()
+        spinnerText.textContent = 'An Error Occured Unable to Fetch Data.....'
+    })
+}, 2000);
+
+backButton.addEventListener('click', () => {
+    history.back()
+})
+
+// night mode
+const nightBtn = document.querySelector('.night-btn')
+const dayBtn = document.querySelector('.day-btn')
+const themeBtn = document.querySelector('.theme-btn')
+
+themeBtn.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn')) {
+        toggleTheme()
+    }
+})
+
+function toggleTheme() {
+    document.querySelector('body').classList.toggle('dark')
+    nightBtn.classList.toggle('hide')
+    dayBtn.classList.toggle('hide')
+
+    if(dayBtn.classList.contains('hide')) {
+        localStorage.setItem('theme', 'light')
+    } else {
+        localStorage.setItem('theme', 'dark')
+    }
+}
+
+function collectingTheme() {
+    if(localStorage.theme === 'dark') {
+        document.querySelector('body').classList.add('dark')
+        nightBtn.classList.add('hide')
+        dayBtn.classList.remove('hide')
+    } else {
+        document.querySelector('body').classList.remove('dark')
+        nightBtn.classList.remove('hide')
+        dayBtn.classList.add('hide')
+    }
+}
+collectingTheme()
+
+
---
+++ script.js
@@ -0,0 +1,160 @@
+const cardContainer = document.querySelector('.card-container')
+const hideSkeleton = () => {
+    document.querySelectorAll('.skeleton-card').forEach((curr) => {
+        curr.remove()
+    })
+}
+
+const showErrorMsg = () => {
+    const errmsg = document.createElement('h2')
+    errmsg.classList.add('errmsg')
+    errmsg.textContent = 'Unable To Fetch Data.....'
+
+    cardContainer.appendChild(errmsg)
+}
+
+function addingskeleton() {
+    const fragment = document.createDocumentFragment()
+    for(let i=0; i<=9; i++)  {
+        const skeletonCard = document.createElement('div')
+        skeletonCard.classList.add('skeleton-card')
+        skeletonCard.innerHTML = `
+            <div class="skeleton flag"></div>
+            <div class="skeleton text title"></div>
+            <div class="skeleton text"></div>
+            <div class="skeleton text"></div>
+            <div class="skeleton text"></div>
+        `;
+        fragment.appendChild(skeletonCard)
+    }
+    cardContainer.innerHTML = ''
+    cardContainer.appendChild(fragment)
+}
+
+const renderdata = (mydata) => {
+    hideSkeleton()
+    const fragment = document.createDocumentFragment()
+    mydata.forEach((country) => {
+        // console.log(country.continents);
+        const card = document.createElement('a')
+        card.classList.add('card')
+        card.href = `./country.html?name=${country.name.common}`
+        // card.target = `_blank`
+        card.dataset.continent = country.continents;
+    
+    
+        const cardDataHtml = `
+            <img src="${country.flags?.svg}" alt="flag-img">
+            <div class="card-text">
+                <h3>${country.name.common}</h3>
+                <p><b>Population:</b> ${country.population.toLocaleString('en-IN')}</p>
+                <p><b>Region:</b> ${country.region}</p>
+                <p><b>Capital:</b> ${(country.capital) ? country.capital : 'N/A'}</p>
+            </div>
+            `
+            card.innerHTML = cardDataHtml
+            fragment.appendChild(card)
+        })
+        cardContainer.appendChild(fragment)
+}
+
+let allCountriesData
+
+setTimeout(() => {
+    fetch('https://restcountries.com/v3.1/all').then((data) => {
+        if(data.status != 200) {
+            hideSkeleton()
+            throw new Error (`ERROR CODE: ${data.status}`)
+        }
+        return data.json()
+    }).then((mydata) => {
+        renderdata(mydata)
+        allCountriesData = mydata
+
+    }). catch((err)=> {
+        hideSkeleton()
+        showErrorMsg()
+    })
+}, 1000);
+
+// search
+const searchbar = document.querySelector('.search-bar')
+searchbar.addEventListener('input', (e) => {
+    const filteredCountry = allCountriesData.filter((country) => {
+       return country.name.common.toLowerCase().includes(e.target.value.trim().toLowerCase())
+    })
+    cardContainer.innerHTML = ''
+    renderdata(filteredCountry)
+    
+    if(filteredCountry.length === 0) {
+        cardContainer.innerHTML = 'No Country Found'
+
+    }
+})
+
+// filtering by region
+document.querySelector('select').addEventListener('change', (e) => {
+    addingskeleton()
+    setTimeout(() => {
+        fetch(`https://restcountries.com/v3.1/region/${e.target.value}`).then((data) => {
+            if(data.status != 200) {
+                hideSkeleton()
+                throw new Error (`ERROR CODE: ${data.status}`)
+            }
+            return data.json()
+        }).then((mydata) => {
+            // console.log(mydata);
+            renderdata(mydata)
+            
+        }). catch((err)=> {
+            cardContainer.innerHTML = ''
+            hideSkeleton()
+            showErrorMsg()
+        })
+    }, 1000)
+})
+
+
+
+//night mode
+const nightBtn = document.querySelector('.night-btn')
+const dayBtn = document.querySelector('.day-btn')
+const theme = document.querySelector('.theme-btn')
+const body = document.querySelector('body')
+theme.addEventListener('click', (e) => {
+    if(e.target.classList.contains('btn')) {
+        toogleTheme()
+    }
+
+})
+
+function toogleTheme() {
+    body.classList.toggle('dark')
+    dayBtn.classList.toggle('hide')
+    nightBtn.classList.toggle('hide')
+    
+    if(body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark')
+    } else {
+        localStorage.setItem('theme', 'light')
+    }
+}
+
+function collectingTheme() {
+    if(!localStorage.theme) {
+        localStorage.setItem('theme', 'light')
+    }
+
+    if(localStorage.theme === 'dark') {
+        body.classList.add('dark')
+        nightBtn.classList.add('hide')
+        dayBtn.classList.remove('hide')
+        // localStorage.setItem('theme', 'dark')
+    } else {
+        body.classList.remove('dark')
+        dayBtn.classList.add('hide')
+        nightBtn.classList.remove('hide')
+        // localStorage.setItem('theme', 'light')
+    }
+}
+collectingTheme()
